2da2ca6b25f553c8bcecc978d127895640466c43
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-03-22 12:58:49 -0400

    Updates contrib-compass. Adds sourcemap option. Still does not reflect adjustments in styles pane of DevTools back to source file

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,8 @@ module.exports = function(grunt) {
         compass: {
             dist: {
                 options: {
-                    config: 'config.rb'
+                    config: 'config.rb',
+                    sourcemap: true
                 }
             }
         }
