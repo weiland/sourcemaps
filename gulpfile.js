85c3a8d85d42e54ce50bf8e288218856f549fef6
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-05-07 22:55:26 -0400

    sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,57 @@
-// Installing Gulp
-// =======================================================
-// References
-// http://gulpjs.com
-// https://laracasts.com/lessons/gulp-this
-// http://markgoodyear.com/2014/01/getting-started-with-gulp
-// https://gist.github.com/grayghostvisuals/e85c85243a79f1ccc58a
-// http://tooling.github.io/book-of-modern-frontend-tooling/build-systems/gulp/getting-started.html
-
-// $ cd ~
-// $ npm install -g gulp
-// $ cd [projects-dir] && cd baseball-app
-// $ npm install
-
-// Installs gulp locally along with the devDependencies 
-// listed in the package.json file.
-// =======================================================
-
-
-// Load Gulp Plugins
+// Plugins
 var gulp = require('gulp'),
-    compass = require('gulp-compass'), // https://github.com/appleboy/gulp-compass
-    livereload = require('gulp-livereload'),
-    lr = require('tiny-lr'),
-    server = lr();
+	compass = require('gulp-compass'), // https://github.com/appleboy/gulp-compass
+	livereload = require('gulp-livereload'),
+	lr = require('tiny-lr'),
+	server = lr(),
+	connect = require('gulp-connect');
 
-// Gulp CSS Task
+
+// CSS
 gulp.task('styles', function() {
-
-    return gulp.src('css/src/styles.scss')
-        .pipe(compass({
-            // config_file: 'config.rb', // https://github.com/appleboy/gulp-compass/issues/24#issuecomment-42016037
-            sourcemap: true,
-            debug : true,
-            css: 'css',
-            sass: 'css/src'
-        }))
-        .pipe(livereload(server))
-        .pipe(gulp.dest('css'));
-
+	return gulp.src('css/src/styles.scss')
+		.pipe(compass({
+			// config_file: 'config.rb', // https://github.com/appleboy/gulp-compass/issues/24#issuecomment-42016037
+			sourcemap: true,
+			debug : true,
+			css: 'css',
+			sass: 'css/src'
+		}))
+		.pipe(livereload(server))
+		.pipe(gulp.dest('css'));
 });
 
-// Gulp HTML Task
+
+// HTML
 gulp.task('markup', function() {
-    return gulp.src('*.html')
-        .pipe(gulp.dest(''))
-        .pipe(livereload(server));
+	return gulp.src('*.html')
+		.pipe(gulp.dest(''))
+		.pipe(livereload(server));
 });
 
-// Gulp Default CLI Task
-gulp.task('default', function() {
 
-    gulp.start('styles');
+// Default Task
+gulp.task('default', ['connect', 'watch']);
 
+
+// Server
+gulp.task('connect', function() {
+	connect.server({
+		root: '.',
+		livereload: false
+	});
 });
 
-// Gulp CLI Watch Task
+
+// Watch
 gulp.task('watch', function() {
+	server.listen(35729, function (err) {
+		if (err) {
+			return console.log(err);
+		}
 
-    // Listen on port 35729
-    server.listen(35729, function (err) {
-        if (err) {
-            return console.log(err);
-        }
-
-        // Watch .scss files
-        gulp.watch('css/src/**/*.scss', ['styles']);
-
-        // Watch html files
-        gulp.watch('*.html', ['markup']);
-    });
+		gulp.watch('css/src/**/*.scss', ['styles']);
+		gulp.watch('*.html', ['markup']);
+	});
 
 });
